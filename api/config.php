e3ee24f4cb7abb564f21ccf5d286960c6ac3e160
bhargavramA3 <bhargavsairam022@gmail.com>
2024-08-12 17:40:52 +0530

    first step

--- api/config.php
+++ api/config.php
@@ -7,7 +7,7 @@ $password = getenv('DB_PASSWORD');
 $dbname = 'defaultdb';
 $port =26663;
 // Create connection
-$conn = new mysqli_connect($servername, $username, $password,$dbname,$port);
+$conn =mysqli_connect($servername, $username, $password,$dbname,$port);
 
 // Check connection
 echo "connection susseccfuk";
